5e04599eff40d03b2e8b250f5f00b6bd7f38e4db
Diego Santos <diegom.santos03@gmail.com>
2025-01-16 15:08:19 -0300

    chore: finalizado projeto de WebApp, para ampliar os conhecimentos sobre as tecnologias: Asp.NET Core Identity + MVC e Web API + SQL Server + EF Core.

--- WebApp.Identity/Program.cs
+++ WebApp.Identity/Program.cs
@@ -14,24 +14,38 @@ var connectionString = "Data Source=localhost\\sqlexpress;" +
                        "Integrated Security=SSPI;" +
                        "TrustServerCertificate=True;";
 
-var migrationAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name; 
+var migrationAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
 
 builder.Services.AddDbContext<MyUserDbContext>(
-    options => options.UseSqlServer(connectionString, 
+    options => options.UseSqlServer(connectionString,
     sql => sql.MigrationsAssembly(migrationAssembly))
 );
 
-builder.Services.AddIdentity<MyUser, IdentityRole>(options => { })
+builder.Services.AddIdentity<MyUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = true;
+
+    //options.Password.RequireDigit = false;
+    //options.Password.RequireNonAlphanumeric = false;
+    //options.Password.RequireUppercase = false;
+    //options.Password.RequireLowercase = false;
+    //options.Password.RequiredLength = 6;
+
+    options.Lockout.MaxFailedAccessAttempts = 3;
+    options.Lockout.AllowedForNewUsers = true;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+})
     .AddEntityFrameworkStores<MyUserDbContext>()
-    .AddDefaultTokenProviders();
+    .AddDefaultTokenProviders()
+    .AddPasswordValidator<DoesNotContainPasswordValidation<MyUser>>();
 
 builder.Services.AddScoped<IUserClaimsPrincipalFactory<MyUser>,
     MyUserClaimsPrincipalFactory>();
 
-builder.Services.Configure<DataProtectionTokenProviderOptions>(options => 
+builder.Services.Configure<DataProtectionTokenProviderOptions>(options =>
     options.TokenLifespan = TimeSpan.FromHours(3));
 
-builder.Services.ConfigureApplicationCookie(options => 
+builder.Services.ConfigureApplicationCookie(options =>
     options.LoginPath = "/Home/Login");
 
 var app = builder.Build();
